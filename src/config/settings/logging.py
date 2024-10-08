d393867dc6b57535224fdf2ce9245edec1a788e9
IWorkForFood <152487755+IWorkForFood@users.noreply.github.com>
2024-10-08 18:37:45 +0700

    Add logging (#1)
    * Add logging
    
    Add mechanism for saving logs
    into the file debug.log
    After stopping the containers
    the file remains at the address
    \\wsl$\docker-desktop\mnt\docker-desktop-disk\data\docker\volumes
    for docker-desktop on windows
    
    * Change entrypoint's title

--- src/config/settings/logging.py
+++ src/config/settings/logging.py
@@ -1,22 +1,61 @@
+import os
+import logging
+import logging.config
+# use this - logging.basicConfig(level=logging.DEBUG), while writing your own logs to catch all the levels
+
+class InfoFilter(logging.Filter):
+    def filter(self, record):
+        return record.levelno == logging.INFO
+
+logs_folder = "logs"
+log_file = os.path.join(logs_folder, 'debug.log')
+
+if not os.path.exists(logs_folder):
+    os.makedirs(logs_folder)
+
+if not os.path.exists(log_file):
+    open(log_file, 'a').close()
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "formatters": {"standard": {"format": "%(asctime)s - %(levelname)s - %(name)s %(message)s"}},
-    "filters": {},
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s %(levelname)s %(name)s %(message)s"
+        }
+    },
+
     "handlers": {
         "console": {
+            "level": "INFO",
             "class": "rich.logging.RichHandler",
+            "formatter": "standard",
+            "filters": []
+
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs/debug.log",
+            "encoding": "utf-8",
+            "formatter": "standard",
+            "filters": ["info_filter"],
         },
     },
     "loggers": {
         logger_name: {
             "level": "INFO",
             "propagate": True,
+        } for logger_name in ("django", "django.request", "django.db.backends", "django.template", "src", "console")
+    },
+
+    "filters": {
+        "info_filter": {
+            "()":"src.config.settings.InfoFilter"
         }
-        for logger_name in ("django", "django.request", "django.db.backends", "django.template", "src")
     },
     "root": {
-        "handlers": ["console"],
+        "handlers": ["console", "file"],
         "level": "INFO",
     },
 }
