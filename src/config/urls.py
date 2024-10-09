2f03be300bc82f00c553cb4eac9c655fbff3f15b
Egor Gurtyakin <egorgurtyakin@gmail.com>
2024-10-09 18:37:17 +0700

    Add src/api/urls urlpatterns to src/config/urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from src.api.urls import urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(urlpatterns)),
 ]
