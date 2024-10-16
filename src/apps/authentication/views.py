72b3c73c8faaa60382b544e521f5609caf0b89a8
Bezizvestnyi <chikovm@bk.ru>
2024-10-16 21:45:45 +0700

    Add registration and authorization

--- src/apps/authentication/views.py
+++ src/apps/authentication/views.py
@@ -1,2 +1,16 @@
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from .api.serializers import RegisterSerializer
+class RegisterView(generics.CreateAPIView):
 
-# Create your views here.
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+
+regView = RegisterView.as_view()
+
+
+
+
+
+
--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from src.apps.authentication.views import regView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/reg", regView, name="reg_api"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
