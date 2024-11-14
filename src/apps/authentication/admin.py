bd7f0918ee6ae49463de76e09b8e982884369100
Bezizvestnyi <chikovm@bk.ru>
2024-11-14 11:04:18 +0700

    Display 'is_verified' in admin-panel

--- src/apps/authentication/admin.py
+++ src/apps/authentication/admin.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+class CustomUserModel(UserAdmin):
+    list_display = ("id", "username", "email", "first_name",
+                    "last_name", "is_verified")
+
+admin.site.register(User, CustomUserModel)
 
 
